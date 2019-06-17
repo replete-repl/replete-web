f59fa7700cdb9c6bd34ce76e11892b36931f64f5
Ray McDermott <ray@mcdermott.be>
2019-06-18 00:42:05 +0200

    :wrench: make the inputs / ouputs more configurable

--- src/replete/editor.cljs
+++ src/replete/editor.cljs
@@ -10,12 +10,6 @@
     [replete.helpers :as helpers]
     [replete.subs :as subs]))
 
-<<<<<<< Updated upstream
-(def ^:private editor-box-style
-  (merge (flex-child-style "1")
-         {:border        "1px solid lightgrey"
-          :border-radius "4px"}))
-=======
 (defn cmirror-eval-box
   "Show results of eval"
   [{:keys [opts cm-opts style]}]
@@ -27,7 +21,6 @@
         [box
          :style style
          :child [cm-eval/cmirror-eval-comp eval-opts]]))))
->>>>>>> Stashed changes
 
 (defn cmirror-edit-box
   "Edit forms with parinfer support"
@@ -43,29 +36,6 @@
          :style edit=style
          :child [cm-edit/cmirror-edit-comp opts]]))))
 
-<<<<<<< Updated upstream
-(defn eval-box
-  "Show results of eval"
-  [edit-opts cm-opts]
-  (let [result (re-frame/subscribe [::subs/eval-result])]
-    (fn []
-      (let [cm-options (merge {:readOnly true} cm-opts)
-            opts0 (merge {:node-id "output"
-                          :changes @result}
-                         edit-opts)
-            opts (assoc opts0 :cm-options cm-options)]
-        [box
-         :style editor-box-style
-         :child [cm-eval/cmirror-eval-comp opts]]))))
-
-(defn edit-box
-  [edit-opts cmirror-opts key-bindings]
-  (let [ed-opts (merge {:node-id "input"} edit-opts)
-        cm-opts (merge {:autofocus true} cmirror-opts)]
-    [v-box :size "100%" :gap "5px"
-     :children
-     [[cmirror-box ed-opts cm-opts]
-=======
 (defn edit-box
   [{:keys [opts cm-opts key-bindings style]}]
   (let [ed-opts (merge {:node-id "input"} opts)
@@ -73,7 +43,6 @@
     [v-box :size "100%" :gap "5px"
      :children
      [[cmirror-edit-box ed-opts cmirror-opts style]
->>>>>>> Stashed changes
       [button
        :class "btn-primary"
        :label (str "Eval")
@@ -100,14 +69,11 @@
    :bottom   "0px"
    :width    "100%"})
 
-<<<<<<< Updated upstream
-=======
 (def ^:private box-style
   (merge (flex-child-style "1")
          {:border        "1px solid lightgrey"
           :border-radius "4px"}))
 
->>>>>>> Stashed changes
 (defn- key-binding
   [key-map [button event]]
   (assoc {} (get key-map button) #(re-frame/dispatch [event])))
@@ -116,11 +82,7 @@
   [key-map event-map]
   (apply merge (map (partial key-binding key-map) event-map)))
 
-<<<<<<< Updated upstream
-(def ^:private event-bindings
-=======
 (def event-bindings
->>>>>>> Stashed changes
   {:enter ::events/eval
    :right ::events/history-next
    :down  ::events/history-next
@@ -135,17 +97,6 @@
                        :preamble-text   replete-preamble-text
                        :preamble-markup preamble-markup}
             eval-cm-opts {:theme "replete-eval-light"}
-<<<<<<< Updated upstream
-            edit-opts {:node-id "replete-input"}
-            extra-keys (extra-key-bindings @key-bindings event-bindings)
-            edit-cm-opts {:theme "replete-edit-light"
-                          :extraKeys extra-keys}]
-        [box
-         :style main-style
-         :child [v-split :initial-split 70
-                 :panel-1 [eval-box eval-opts eval-cm-opts]
-                 :panel-2 [edit-box edit-opts edit-cm-opts @key-bindings]]]))))
-=======
             eval-box-opts {:opts    eval-opts
                            :cm-opts eval-cm-opts
                            :style   box-style}
@@ -163,6 +114,5 @@
          [v-split :initial-split 70
           :panel-1 [cmirror-eval-box eval-box-opts]
           :panel-2 [edit-box edit-box-opts]]]))))
->>>>>>> Stashed changes
 
 
